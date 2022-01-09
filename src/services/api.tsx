b522fc3eab69a4df587c9d0ee5a5bdfc9f40667e
Davi S.P <davisp2009@hotmail.com>
2022-01-09 20:43:11 -0300

    Deploy Heroku

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({baseURL: 'http://localhost:8000/'})
+const api = axios.create({baseURL: 'http://ec2-15-229-10-69.sa-east-1.compute.amazonaws.com:8000/'})
 
 export default api
